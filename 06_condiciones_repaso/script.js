035b8445ffe2b6883d8260ed825ac67518c1c448
1B0v0 <201227420+benjamindelgado16@users.noreply.github.com>
2025-10-15 14:45:10 -0300

    .

--- 06_condiciones_repaso/script.js
+++ 06_condiciones_repaso/script.js
@@ -48,10 +48,68 @@ function compararDosNumeros() {
 
 function parImpar() {
     let par = parseInt(prompt("Coloque un número"));
-    let impar = parseInt(prompt("Coloque otro número"));
-    if (par % impar) {
+    if (par % 2 == 0) {
         alert(`Es par`)
     } else {
         alert(`Es impar`)
+    }
+}
+
+function palabraInicial() {
+    let letraA = prompt("Coloque una palabra");
+    if (letraA[0].toLowerCase() === "a") {
+        alert("La palabra empieza con a")
+    } else {
+        alert("La palabra no empieza con a ")
+    }
+}
+
+function temperatura() {
+    let ambiente = parseInt(prompt("Coloque la temperatura"));
+    if (ambiente < 10) {
+        alert("Hace frio")
+    } else if (ambiente >= 10 && ambiente <= 25) {
+        alert("Esta fresco")
+    } else if (ambiente >= 25 && ambiente < 42) {
+        alert("Hace calor") 
+    } else if (ambiente > 42) {
+        alert("Hace demasiado calor")
+    } else {
+        alert("Error")
+    }
+}
+
+function nombreReconocido() {
+    let name1 = "Dany";
+    let name2 = "Ely";
+    let nombre = prompt("Coloque un nombre");
+    if (nombre === name1) {
+        alert("Hola profesor")
+    } else if (nombre === name2) {
+        alert("Hola Mamá")
+    } else {
+        alert("Hola visitante")
+    }
+}
+
+function nota() {
+    let notas = parseFloat(prompt("Coloque su Nota")); 
+    if (notas < 4) {
+        alert("Reprobado");
+    } else if (notas >= 4 && notas <= 5) {
+        alert("Buena")
+    } else if (notas >= 6 && notas <= 7) {
+        alert("Excelente")
+    } else {
+        alert("Error")
+    }
+}
+
+function palabraClave() {
+    let clave = prompt("Coloque una frase")
+    if(clave.toLowerCase().includes("jesus")) {
+        alert("Contiene la palabra clave")
+    } else {
+        alert("La frase no contiene la palabra clave")
     }
 }
