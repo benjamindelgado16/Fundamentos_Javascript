1d2ae99236a7490ee6a52183c96f423987ded766
1B0v0 <201227420+benjamindelgado16@users.noreply.github.com>
2025-10-20 09:27:31 -0300

    .

---
+++ 07_bucles/script.js
@@ -0,0 +1,42 @@
+function ejecutarWhile() {
+    let resultado = "El resultado es: ";
+    let count = 10;
+    while (count > 0) {
+        resultado += ` ${count}`;
+        count--
+    }
+    alert(resultado)
+}
+
+
+function ejecutarFor() {
+    let resultado = "El resultado es: ";
+
+    for(let i = 10; i > 0; i--) {
+        resultado += ` ${i}`
+    }
+    alert(resultado);
+}
+
+
+function numero() {
+    let resultado = "Contamos: ";
+    let a = parseInt(prompt("Coloque un número: "));
+    count = 1;
+    while(count <= a) {
+        resultado += ` ${count}`
+        console.log(count)
+        count ++;
+    }
+    alert(resultado)
+}
+
+function numero2() {
+    let resultado = "El resultado es: ";
+    let a = parseInt(prompt("Coloque un número: "));
+    while(a > 0) {
+        resultado += ` ${a}`;
+        a--
+    }
+    alert(resultado)
+}
