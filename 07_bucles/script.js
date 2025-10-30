1abf53fd1a22472a58aa40de221de8429d28c9ab
1B0v0 <201227420+benjamindelgado16@users.noreply.github.com>
2025-10-30 15:06:38 -0300

    .

--- 07_bucles/script.js
+++ 07_bucles/script.js
@@ -74,8 +74,64 @@ Pide un número al usuario e imprime su tabla de multiplicar del 1 al 10 en un s
 function multiplicar() {
     let mul = parseInt(prompt("Coloque un número: "));
     let resultado = `Tabla de ${mul}: `
-    for(let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         resultado += `\n ${mul} x ${i} = ${mul * i}`
+    }
+    alert(resultado)
+}
+
+
+/* 6️⃣ Bucle for: Números pares hasta n
+Solicita un número n e imprime todos los números pares desde 1 hasta n usando un bucle for.
+
+Ejemplo de resultado:
+2, 4, 6, 8, 10 */
+
+function pares() {
+    let num = parseInt(prompt("Coloca un número: "));
+    let resultado = "El resultado es: "
+    for (let i = 1; i <= num; i++) {
+        if (i % 2 == 0) {
+            resultado += `  ${i} `
+        }
+    }
+    alert(resultado)
+}
+
+
+/* 7️⃣ Bucle for: Contar letras de una palabra
+Pide una palabra al usuario 
+y muestra cuántas letras tiene usando un bucle for para recorrerla.*/
+
+function contarLetras() {
+    let palabra = prompt("Coloque una palabra: ");
+    let cont = 0
+    for (let i = 0; i < palabra.length; i++) {
+        cont++
+    }
+    alert(`La palabra "${palabra} tiene ${cont} letras"`)
+}
+
+function factorial() {
+    let numero = parseInt(prompt("Ingrese un número para mostras su factorial: "));
+    let factorial = 1;
+    let largo = "1";
+    for (let i = 2; i <= numero; i++) {
+        largo += ` * ${i}`
+        factorial *= i;
+    }
+    alert(`El factorial de ${numero} es: ${largo} = ${factorial}`)
+}
+
+function sumaImpares() {
+    let num = parseInt(prompt("Ingresar número: "));
+    let resultado = "El resultado es: "
+    let suma = 0
+    for (let i = 0; i <= num; i++) {
+        if (i % 2 != 0) {
+            resultado += ` + ${i}`
+            suma += i
+        }
     }
     alert(resultado)
 }
