a083d4ba34c1a73ee86864b4c27cfd84b46a13b4
1B0v0 <201227420+benjamindelgado16@users.noreply.github.com>
2025-12-01 09:30:20 -0300

    .

---
+++ 1.15 Ejercicios-Array/static/js/script.js
@@ -0,0 +1,25 @@
+
+// Ejercicio Número 1
+    let frase = ["Juan", "corre", "rápidamente", "feliz", "parque"];
+
+function construirFrase() {
+    alert(`Frase: ${frase[0]} ${frase[1]} ${frase[2]}  ${frase[3]} por el ${frase[4]}`);
+};
+
+let datos = [3, "gatos", "negros", "duermen", "tranquilamente"];
+
+function gatos() {
+    alert(`Frase: De los 5 gatos, ${datos[0]} ${datos[1]} ${datos[2]} ${datos[3]} ${datos[4]}`);
+};
+
+
+
+
+function sumarElementos() {
+    let matriz = [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ];
+    alert(`Al acceder a la matriz se puede sumar ${matriz}`)
+}
---
+++ arreglos/script.js
@@ -0,0 +1,19 @@
+//practica 
+let colores = ["rojo", "azul", "verde"];
+for(let i = 0; i < colores.length; i++) {
+    console.log("")
+}
+
+let matriz = [
+    [
+        [1,2,3],
+        [4, 5, 6],
+        [7, 8, ["Hola", 2]]
+    ]
+    [
+        11, 12, 13
+    ]
+];
+
+x[1][2]
+x[0][2][2][0]
